fd599f362be246beae62ffa567df916e6575f356
kame <tentyo0620@icloud.com>
2023-02-12 19:50:26 +0900

    apply lint formatter

--- src/pages/403.tsx
+++ src/pages/403.tsx
@@ -1,13 +1,11 @@
 import CustomErrorBox from '@/components/organisms/CustomErrorBox';
 
-const Custom403 = () => {
-  return (
-    <CustomErrorBox
-      statusCode="403"
-      message="このページにアクセスする権限がありません"
-      description={<>URLにタイプミスがないか再度ご確認ください。</>}
-    />
-  );
-};
+const Custom403 = () => (
+  <CustomErrorBox
+    statusCode="403"
+    message="このページにアクセスする権限がありません"
+    description={<>URLにタイプミスがないか再度ご確認ください。</>}
+  />
+);
 
 export default Custom403;
--- src/pages/500.tsx
+++ src/pages/500.tsx
@@ -1,19 +1,17 @@
 import CustomErrorBox from '@/components/organisms/CustomErrorBox';
 
-const Custom500 = () => {
-  return (
-    <CustomErrorBox
-      statusCode="500"
-      message="ページが表示できません"
-      description={
-        <>
-          サーバーで問題が発生しているためページが表示できません。
-          <br />
-          しばらく時間を置いてからやり直してください。
-        </>
-      }
-    />
-  );
-};
+const Custom500 = () => (
+  <CustomErrorBox
+    statusCode="500"
+    message="ページが表示できません"
+    description={
+      <>
+        サーバーで問題が発生しているためページが表示できません。
+        <br />
+        しばらく時間を置いてからやり直してください。
+      </>
+    }
+  />
+);
 
 export default Custom500;
