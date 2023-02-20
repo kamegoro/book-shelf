a2e9684e37f7f851793c626278d917096c5b879f
kamegoro <yuya.kashihara@x-factory.jp>
2023-02-20 20:34:27 +0900

    Added specification that the progress bar is displayed during page transitions

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,12 @@
 import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
+import Router from 'next/router';
+import nProgress from 'nprogress';
+import 'nprogress/nprogress.css';
+
+Router.events.on('routeChangeStart', () => nProgress.start());
+Router.events.on('routeChangeError', () => nProgress.done());
+Router.events.on('routeChangeComplete', () => nProgress.done());
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
