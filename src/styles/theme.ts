aaca61370d828bae80397f98b93ad65c39291f80
kame <tentyo0620@icloud.com>
2023-02-23 16:22:44 +0900

    Add mui theme file

---
+++ src/styles/theme.ts
@@ -0,0 +1,13 @@
+import { createTheme } from '@mui/material';
+
+export default createTheme({
+  breakpoints: {
+    values: {
+      xs: 0,
+      sm: 600,
+      md: 768,
+      lg: 1025,
+      xl: 1536,
+    },
+  },
+});
